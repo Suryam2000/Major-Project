719e5e9d1066e50be9acffa1e04786415bab1464
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-07-29 20:14:18 +0530

    Rectified session error on server restart using mongo store

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded());
 
@@ -29,7 +30,16 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl: 'mongodb://localhost:27017/major_project',
+            autoRemove: 'disabled'
+        },
+        function(err){
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
