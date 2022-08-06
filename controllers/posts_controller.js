e79324a3c0d965426879abfc910b2d8aff93a62f
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-08-07 00:00:45 +0530

    Added delete post option for specific user of the post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Posts = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.createPost = function(req, res){
     Posts.create({
@@ -9,4 +10,20 @@ module.exports.createPost = function(req, res){
 
         return res.redirect('back');
     }); 
+};
+
+module.exports.destroy = function(req, res){
+    Posts.findById(req.params.id, function(err, post){
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){console.log(err); return;}
+
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
 };
