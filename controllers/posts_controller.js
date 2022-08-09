67b5c60593438f0c4fed839209e1c4fdc3a13a4e
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-08-10 01:08:05 +0530

    Added ajax to create and delete posts and sorted posts in creating wise

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,19 @@ const Comment = require('../models/comment');
 
 module.exports.createPost = async function(req, res){
     try {
-        await Posts.create({
+        let post = await Posts.create({
             content: req.body.content,
             user: req.user._id
         }); 
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created!"
+            });
+        }
 
         req.flash('success', 'Posted Successfully');
     
@@ -22,8 +31,20 @@ module.exports.destroy = async function(req, res){
 
         if(post.user == req.user.id){
             post.remove();
+
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post Deleted!"
+                });
+            }
+
             req.flash('success', 'Post Deleted!');
+
             return res.redirect('back');
         }else{
             return res.redirect('back');
