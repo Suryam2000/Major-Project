04f13e13baad30ecc7ed19d076d56d8ee43d3018
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-09-19 00:49:58 +0530

    Coded socket connection part

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,8 @@
 module.exports.chatSockets = function(socketServer){
-    
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', function(socket){
+        console.log('New connection recieved', socket.id);
+    });
+
 }
